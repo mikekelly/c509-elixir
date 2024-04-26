062c0fedd9eb671fa64cca00d56a1b0d8c796f18
Mike Kelly <mikekelly321@gmail.com>
2024-04-26 12:09:27 +0100

    mix new c509

---
+++ lib/c509.ex
@@ -0,0 +1,18 @@
+defmodule C509 do
+  @moduledoc """
+  Documentation for `C509`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> C509.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/c509_test.exs
@@ -0,0 +1,8 @@
+defmodule C509Test do
+  use ExUnit.Case
+  doctest C509
+
+  test "greets the world" do
+    assert C509.hello() == :world
+  end
+end
