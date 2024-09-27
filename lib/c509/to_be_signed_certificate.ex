d78337d31428b7de235973efbf947281f81e0d55
Mike Kelly <mikekelly321@gmail.com>
2024-09-27 22:48:55 +0100

    encode the serial number as per spec

--- lib/c509/to_be_signed_certificate.ex
+++ lib/c509/to_be_signed_certificate.ex
@@ -22,7 +22,7 @@ defmodule C509.ToBeSignedCertificate do
   defp bytes_to_sign(tbs_cert) do
     cbor_items = [
       tbs_cert.type,
-      tbs_cert.serial_number,
+      %CBOR.Tag{tag: :bytes, value: encode_serial_number(tbs_cert.serial_number)},
       tbs_cert.issuer,
       tbs_cert.valid_not_before,
       tbs_cert.valid_not_after,
@@ -36,5 +36,22 @@ defmodule C509.ToBeSignedCertificate do
     cbor_items
     |> Enum.map(&CBOR.encode/1)
     |> Enum.join
+  end
+
+  defp encode_serial_number(serial_number) do
+    lstrip_zero_bytes(:binary.encode_unsigned(serial_number))
+  end
+
+  defp lstrip_zero_bytes(binary) when is_binary(binary) do
+    do_lstrip_zero_bytes(binary)
+  end
+
+  # Recursive helper function to strip leading zero bytes
+  defp do_lstrip_zero_bytes(<<0, rest::binary>>) do
+    do_lstrip_zero_bytes(rest)
+  end
+
+  defp do_lstrip_zero_bytes(other) do
+    other
   end
 end
